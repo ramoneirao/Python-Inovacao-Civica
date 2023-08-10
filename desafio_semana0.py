c849fcf06a0e8bf5ec6ff45ca4ea32cae7ebfa74
Ramon Mendes <ramonneirao@gmail.com>
2023-08-10 14:39:17 -0300

    Desafio Semana 0

--- desafio_semana0.py
+++ desafio_semana0.py
@@ -1,13 +1,4 @@
-"""
-Faça um programa que Calcule a soma de três números inteiros. O programa deve exibir na tela uma mensagem como a indicada abaixo de exemplo:
-
-OBS: os valores 15, 10, 30 representam números de exemplo. Seu programa deve funcionar para quaisquer outros valores.
-
-    Entrada: 15, 10, 30
-    Processamento: 15 + 10 + 30
-    Saída: 55
-
-"""
+#Faça um programa que Calcule a soma de três números inteiros. O programa deve exibir na tela uma mensagem como a indicada abaixo de exemplo:
 num1 = int(input('Digite o 1º número: '))
 num2 = int(input('Digite o 2º número: '))
 num3 = int(input('Digite o 3º número: ')) 
